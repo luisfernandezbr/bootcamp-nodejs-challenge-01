3569664a497087b0cbf3569d47cbb0e9ffdb92ee
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2019-02-24 15:42:34 -0300

    Initing boilerplate with Express, Nodemon, Nunjucks, ESLint and EditorConfigs

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const app = express()
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  watch: true
+})
+
+app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'njk')
+
+app.listen(3000)
