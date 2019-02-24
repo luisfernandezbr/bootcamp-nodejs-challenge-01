4afeecdaf441b7374a40f49e7addd91bfce3bd44
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2019-02-24 16:10:19 -0300

    Adding age verification and page results

--- index.js
+++ index.js
@@ -16,4 +16,26 @@ app.get('/', (req, res) => {
   return res.render('home')
 })
 
+app.post('/check', (req, res) => {
+  const userAge = req.body.age
+
+  if (userAge >= 18) {
+    return res.redirect(`major?age=${userAge}`)
+  } else {
+    return res.redirect(`minor?age=${userAge}`)
+  }
+})
+
+app.get('/major', (req, res) => {
+  return res.render('major', {
+    age: req.query.age
+  })
+})
+
+app.get('/minor', (req, res) => {
+  return res.render('minor', {
+    age: req.query.age
+  })
+})
+
 app.listen(3000)
